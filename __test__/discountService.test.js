19122b44748badbce68acee4ac4c7ddac8ca64fc
artumarinn <artumarin.09@gmail.com>
2024-12-21 21:54:39 -0300

    Test the API GET Request

--- __test__/discountService.test.js
+++ __test__/discountService.test.js
@@ -1,5 +1,12 @@
+const request = require('supertest');
 
-test('Sample test', () => {
-    expect(true).toBe(true);  
-  });
+// Test the API GET Request
+test('API GET Request Test', async () => {
+  const response = await request('http://localhost:3000').get('/api/v1/');
+
+  console.log(response.status); 
+  console.log(response.body);
   
+  expect(response.status).toBe(200);
+  expect(response.body[0].id).toBe(1);
+});
