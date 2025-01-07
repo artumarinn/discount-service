e65ba6fad82020630bc789ce4d4e28114c9e49b8
artumarinn <artumarin.09@gmail.com>
2025-01-07 15:50:14 -0300

    Request Test

--- __test__/discountService.test.js
+++ __test__/discountService.test.js
@@ -10,3 +10,33 @@ test('API GET Request Test', async () => {
   expect(response.status).toBe(200);
   expect(response.body[0].id).toBe(1);
 });
+
+// Test the API POST Request
+test('API POST Request Test', async () => {
+  const response = await request('http://localhost:3000').post('/api/v1/').send({
+    code: "DISCOUNT2025",
+    discount_percent: 800,
+    is_active: true,
+    valid_until: "2025-11-31T23:59:59.000Z"
+  });
+
+  console.log(response.status); 
+  console.log(response.body);
+  
+  expect(response.status).toBe(201);
+});
+
+// Test the API PUT Request
+test('API PUT Request Test', async () => {
+  const response = await request('http://localhost:3000').put('/api/v1/1').send({
+    code: "DISCOUNT2025",
+    discount_percent: 30,
+    is_active: true,
+    valid_until: "2025-05-31T23:59:59.000Z"
+  });
+
+  console.log(response.status); 
+  console.log(response.body);
+  
+  expect(response.status).toBe(200);
+});
